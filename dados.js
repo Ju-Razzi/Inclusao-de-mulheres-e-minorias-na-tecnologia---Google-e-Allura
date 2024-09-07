de938cc54a39d86a9c7c7d0463e28e3e0396d51c
Ju-Razzi <julianamatarazzi@gmail.com>
2024-09-06 22:11:47 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,32 @@
+let dados = [
+    {
+        titulo: "Programaria",
+        descricao: "A PrograMaria desenvolve cursos online de primeiros passos na tecnologia para quem não sabe nada, com uma metodologia cheia de mão na massa, prazerosa e em um ambiente acolhedor. São uma comunidade poderosa: mais de 11.000 astronautas, como carinhosamente chamam as pessoas alunas, que passaram pelo curso e relatam que essa experiência não só aumentou a autoconfiança para aprender a programar, como ajudou na transição de carreira para tecnologia. Para conseguir ajudar pessoas que não conseguem arcar com o custo da matrícula, oferecem bolsas priorizando pessoas negras, trans e travestis. Este fundo é composto pelas próprias matrículas das alunas, e também por empresas.",
+        link: "https://www.programaria.org/",
+        tags: "curso gratuito negras trans travestis"
+    },
+    {
+        titulo: "Reprograma",
+        descricao: "Oferecem cursos de programação completamente gratuitos para mulheres e adolescentes em situações de vulnerabilidade social, econômica e de gênero, priorizando nos processos seletivos pessoas negras, trans e travestis.",
+        link: "https://reprograma.com.br/",
+        tags: "mulheres adolescentes situação de vulnerabilidade social"
+    },
+    {
+        titulo: "Minas Programam",
+        descricao: "O Minas Programam é uma iniciativa criada em 2015 para desafiar os estereótipos de gênero e de raça que influenciam nossa relação com as áreas de ciências, tecnologia e computação. Promovem oportunidades de aprendizado sobre programação para meninas e mulheres, priorizando àquelas que são negras ou indígenas.",
+        link: "https://minasprogramam.com/",
+        tags: "genero raça negras indigenas"
+    },
+    {
+        titulo: "WoMakersCode",
+        descricao: "“WoMakersCode” é outra iniciativa brasileira com foco em promover a diversidade de gênero na área de tecnologia, especialmente no campo da programação. A organização busca proporcionar oportunidades de aprendizado, networking e empoderamento para mulheres interessadas em ingressar ou avançar em suas carreiras na indústria de TI.",
+        link: "https://womakerscode.org/",
+        tags: "diversidade programação networking"
+    },
+    {
+        titulo: "Descodificadas",
+        descricao: "O Programa Descodificadas é feito para mulheres cis e pessoas trans +16 tendo a duração de 2 anos para a formação completa sendo composto por 4 trilhas de aprendizagem, cada uma delas com duração de 6 meses. As trilhas de aprendizagem são compostas por dois componentes que envolvem a formação específica na área desenvolvimento web e um componente de competências socioemocionais que visa o autodesenvolvimento.",
+        link: "https://descodificadas.com.br/",
+        tags: "cis trans desenvolvimento web"
+    }
+];
